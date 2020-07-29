49b11b74a75acfc121fb1484260d632102f6f61b
viniagostini <viniciusaagostini@gmail.com>
2020-07-28 21:08:24 -0300

    add script to add products to staticPaths.json

---
+++ n-pages-experiment/addPaths.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-console */
+const fs = require('fs')
+
+const getJson = (relativePath) =>
+  JSON.parse(fs.readFileSync(`${__dirname}/${relativePath}`))
+
+const run = (nProducts) => {
+  const products = getJson(`products.json`)
+  const currentPaths = getJson(`../staticPaths.json`)
+
+  const selectedProducts = products.filter((product, i) => i < nProducts)
+
+  const result = [...currentPaths, ...selectedProducts]
+
+  fs.writeFileSync(
+    `${__dirname}/../staticPaths.json`,
+    JSON.stringify(result, undefined, 2)
+  )
+}
+
+const n = process.env.PRODUCTS
+
+run(n)
