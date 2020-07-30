cf889528c729bb5cbbfd2f42d16b2414933c5b2f
Victor Elias <victorgelias@gmail.com>
2020-07-30 18:54:02 -0300

    Feat/fast pocking (#3)
    * Change mode of scripts
    
    * Create setupNode.sh script
    
    * Automate tmpfs size to half system memory
    
    * Fix yarn and json2csv installation
    
    * Fix tmpfsSize unit
    
    * Convert output to CSV
    
    * Add simpler init.sh script to run right after clone
    
    * Add blank line at eof

---
+++ n-pages-experiment/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./setupNode.sh
+./setupYarn.sh
---
+++ n-pages-experiment/setupNode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+# Mount local SSD
+df -h
+lsblk
+sudo mkfs /dev/nvme1n1
+sudo mkdir -p /poc/local-ssd
+sudo chown ec2-user /poc
+sudo mount /dev/nvme1n1 /poc/local-ssd
+sudo chown ec2-user /poc/local-ssd
+ls -l /poc/local-ssd
+
+# Mount tmpfs
+sysmem=$(cat /proc/meminfo | grep MemTotal | awk -F ' ' '{print $2}')
+tmpfsSize=$(expr $sysmem / 2)
+
+mkdir -p /poc/tmpfs
+sudo mount -t tmpfs -o size=${tmpfsSize}K tmpfs /poc/tmpfs
+sudo chown ec2-user /poc/tmpfs
+ls -l /poc/tmpfs
+
+# Install tools
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install node
+npm install -g yarn
+npm install -g json2csv
