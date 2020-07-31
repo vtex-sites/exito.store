6256b5eac0bf11151e61f2279e116c359f173676
dhasuda <davihasuda@gmail.com>
2020-07-31 15:27:03 -0300

    Conditional build experiment

--- conditional-build-experiment/runExperiment.sh
+++ conditional-build-experiment/runExperiment.sh
@@ -2,18 +2,18 @@
 
 export NODE_OPTIONS=--max_old_space_size=8192
 
-END=1
+END=5
 
-yarn --cwd ./../ clean
 echo "Testing simple build ✨✨✨✨✨"
 for ((i=1;i<=END;i++)); do
-  yarn --cwd ./../ simple-build
+  yarn --cwd ./../ clean
+  yarn --cwd ./../ simple-build | tee ./raw-results/simple-result-$i.txt
 done
 echo "Simple build test ended ✨✨✨✨✨"
 
 echo "Testing conditional build 🔪🔪🔪🔪"
 yarn --cwd ./../ clean
 for ((i=1;i<=END;i++)); do
-  yarn --cwd ./../ conditional-build
+  yarn --cwd ./../ conditional-build | tee ./raw-results/conditional-result-$i.txt
 done
 echo "Conditional build test ended 🔪🔪🔪🔪"
