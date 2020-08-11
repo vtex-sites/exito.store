a66a7cde8c81d3404c304bd9e47ceabdb8751f15
viniagostini <viniciusaagostini@gmail.com>
2020-08-11 15:33:28 -0300

    just something to test the PR pipelline

--- n-pages-experiment/setupYarn.sh
+++ n-pages-experiment/setupYarn.sh
@@ -2,6 +2,9 @@ yarn --cwd ./../../gatsby-vtex
 yarn --cwd ./../../gatsby-vtex/packages/gatsby-source-vtex
 yarn --cwd ./../../gatsby-vtex/packages/gatsby-source-vtex link
 
+
+
+
 yarn --cwd ./../
 yarn --cwd ./../ link "@vtex/gatsby-source-vtex"
 
